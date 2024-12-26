2a25e98280c2ac5a9281349cb1bbf1ddd9eb4b54
Virtualstar <Virtualstar0125@gmail.com>
2024-12-26 14:29:35 +0800

    sth

--- unihive/forum/models.py
+++ unihive/forum/models.py
@@ -19,29 +19,28 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
-# # TODO: User properties to be refined
-# class User():
-#     username = models.TextField()
+class User():
+    username = models.TextField()
 
 
-# class Post(models.Model):
-#     picture = models.ImageField(upload_to=post_picture_path)
-#     content = models.TextField()
-#     time = models.DateTimeField(auto_now_add=True)
-#     likes = models.ManyToManyField(User, blank=True, related_name='liked')
-#     pinned = models.BooleanField(default=False)
+class Post(models.Model):
+    picture = models.ImageField(upload_to=post_picture_path)
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True, related_name='liked')
+    pinned = models.BooleanField(default=False)
 
 
-# class Profile(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     followers = models.ManyToManyField(User, blank=True, related_name="following")
-#     liked = models.ManyToManyField(User, blank=True, related_name="Times to be liked")
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    followers = models.ManyToManyField(User, blank=True, related_name="following")
+    liked = models.ManyToManyField(User, blank=True, related_name="Times to be liked")
     
 
-# class Message(models.Model):
-#     text = models.TextField()
-#     time = models.DateTimeField(default=now)
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_message")
-#     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_message")
+class Message(models.Model):
+    text = models.TextField()
+    time = models.DateTimeField(default=now)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_message")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_message")
     
 
